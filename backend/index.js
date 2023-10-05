a8134ebc5ab4dce3ae804cc05b689a04919aea7a
vignesh <vigneshshet4577@gamil.com>
2023-10-05 17:56:55 +0530

    till 44

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,29 @@
 const connectToMongo = require("./db");
-// const express = require("express");
+const express = require("express");
 // const mongoose = require("mongoose");
 
 connectToMongo();
 
 // mongoose.connect("mongodb://localhost:27017/local");
 
-// const app = express();
-// const port = 3000;
+const app = express();
+const port = 3000;
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port http://localhost:${port}`);
-// });
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
+// app.get("/signin", (req, res) => {
+//     res.send("Hello sign in!");
+//   });
+
+//   app.get("/login", (req, res) => {
+//     res.send("Hello login!");
+//   });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
