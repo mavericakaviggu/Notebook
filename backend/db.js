187c77ba7bada01139da6ef4e4c9ce8ba8e2e821
vignesh <vigneshshet4577@gamil.com>
2023-10-08 18:05:16 +0530

    till 46

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 // const { MongoClient } = require("mongodb");
-const mongoURI = "mongodb://127.0.0.1:27017/";
+const mongoURI = "mongodb://127.0.0.1:27017/notebook";
 // const client = new MongoClient(mongoURI);
 
 const connectToMongo = async () => {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User')
+const{body, validationResult} = require ("express-validator")//For data calidation we need to install this
 
 
 //Create a user using POST "api/auth/".Doesn't require Auth
-router.get('/',(req,res)=>{
-    console.log(req.body)
-    const user = User(req.body)
-    user.save()
-    res.send(req.body)
+router.post('/',[
+    //below are validation rules
+    body('name','Enter a valid name').isLength({min:3}),
+    body('email','Enter a valid email').isEmail(),
+    body('password','Enter a valid password').isLength({min:5}),
+],
+(req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+    User.create({
+        name: req.body.name,
+        email:req.body.email,
+        password: req.body.password,
+        }).then(user => res.json(user))
+        .catch(err=>{console.log(err)
+    res.json({error:'Please enter a unique value for email',message:err.message})});
 });
 
 module.exports = router
