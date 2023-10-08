c0cbf33f3a5a1d89b9ca56bf80d5bf77631915c3
vignesh <vigneshshet4577@gamil.com>
2023-10-08 13:25:58 +0530

    till 44

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,9 @@ connectToMongo();
 const app = express();
 const port = 3000;
 
+//Helps in viewing json fies through thunderstorm clinet(postman)
+app.use(express.json())
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User')
 
+
+//Create a user using POST "api/auth/".Doesn't require Auth
 router.get('/',(req,res)=>{
-    const obj={
-        a:'thios',
-        number:34
-    }
-    res.json(obj)
+    console.log(req.body)
+    const user = User(req.body)
+    user.save()
+    res.send(req.body)
 });
 
 module.exports = router
