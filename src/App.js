844680c26eb87f14b24eda26847951195f5f57ae
vignesh <vigneshshet4577@gamil.com>
2023-10-14 12:38:27 +0530

    till 56

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
+import { Home } from './components/Home';
+import { About } from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is notebook cient side</h1>
-    </div>
+    <>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route
+          path="/home"
+          element={
+            <Home/>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <About/>
+          }
+        />
+        {/* <Route
+          path="/"
+          element={
+            <Home/>
+          }
+        /> */}
+      </Routes>
+    </Router>
+     
+    </>
   );
 }
 
